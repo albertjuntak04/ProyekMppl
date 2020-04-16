0cd74bf13ab06a71b562323f1487722322f221da
Albert <albertsimanjuntak02@gmail.com>
2020-04-16 23:35:12 +0700

    edit belum sempurna

--- app/src/main/java/com/example/projectmppl/activity/MainActivity.java
+++ app/src/main/java/com/example/projectmppl/activity/MainActivity.java
@@ -1,19 +1,14 @@
 package com.example.projectmppl.activity;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import android.content.DialogInterface;
-import android.content.Intent;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.projectmppl.fragment.HomeFragment;
 import com.example.projectmppl.R;
-import com.example.projectmppl.fragment.ProfileFragment;
 import com.example.projectmppl.fragment.RiwayatFragment;
 import com.example.projectmppl.fragment.TentangFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
